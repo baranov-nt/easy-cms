7feea9d44d37a072dd5983b41b8312b798c75c9e
phpnt <admin@phpnt.com>
2016-09-24 02:20:27 +0500

    first commit

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=5.45.124.29;dbname=easy-cms',
+    'username' => 'setyes',
+    'password' => 'a748b5rk9',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
